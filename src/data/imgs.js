f41b771dc1faf70a0fd7c0c91d15d042fcc631e9
Abdelmoniem Ahmed Hgagy <137680518+Abdelmoniemhgagyy@users.noreply.github.com>
2025-01-24 07:31:24 +0200

    add shuffle and some data

--- src/data/imgs.js
+++ src/data/imgs.js
@@ -1,4 +1,4 @@
-
+import shuffleArray from "../components/shuffleArray"
 const imgs = [
     { id: 1, title: "1", path: "/imgs/4.jpg" },
     { id: 2, title: "c", path: "/imgs/2.jpg" },
@@ -14,11 +14,11 @@ const imgs = [
     { id: 12, title: "l", path: "/imgs/12.jpg" },
     { id: 13, title: "13", path: "/imgs/13.jpg" },
     { id: 14, title: "n", path: "/imgs/14.jpg" },
-    // { id: 15, title: "15", path: "/imgs/15.jpg" },
-    // { id: 16, title: "p", path: "/imgs/16.jpg" },
-    // { id: 17, title: "17", path: "/imgs/17.jpg" },
-    // { id: 18, title: "r", path: "/imgs/18.jpg" },
+    { id: 15, title: "15", path: "/imgs/15.jpg" },
+    { id: 16, title: "p", path: "/imgs/16.jpg" },
+    { id: 17, title: "17", path: "/imgs/17.jpg" },
+    { id: 18, title: "r", path: "/imgs/18.jpg" },
     // { id: 19, title: "19", path: "/imgs/19.jpg" },
     // { id: 20, title: "t", path: "/imgs/20.jpg" },
 ]
-export default imgs
+export default shuffleArray(imgs)
